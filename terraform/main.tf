9869745a2ed8f2d6d9158670be84aed32c6c5bd9
Raffaele Marcello <marcelloraffaele@gmail.com>
2024-04-24 00:12:07 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,25 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+
+//https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/eventhub.html
+
+resource "azurerm_eventhub_namespace" "eh_namespace" {
+  name                = "acceptanceTestEventHubNamespace"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  capacity            = 1
+
+  tags = var.resource_tags
+}
+
+resource "azurerm_eventhub" "eh" {
+  name                = "acceptanceTestEventHub"
+  namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 1
+  message_retention   = 1
+}
+
+//https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
   value = data.azurerm_resource_group.rg.name
 }
+
+//output "azurerm_eventhub" {
+//  value = data.azurerm_eventhub.
+//}
